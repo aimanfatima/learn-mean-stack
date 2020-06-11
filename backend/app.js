7d818d62b6735b3db2d4524a0b978f6c0070ab56
Aiman Fatima <aimanfatimadl@gmail.com>
2020-06-11 23:01:06 +0530

    Add first route: server side,  with dummy posts

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = new express();
 
-app.use((req, res, next) => {
-  console.log('First Middleware');
-  next();
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "1234",
+      title: "First server side title",
+      content: "First server side content"
+    },
+    {
+      id: "5678",
+      title: "Second server side title",
+      content: "Second server side content"
+    }
+  ];
+  res.status(200).json({
+    message: "Post fetched successfully!",
+    posts: posts
+  });
 })
-
-app.use((req, res, next) => {
-  res.send("Hello from express!!");
-})
-
 module.exports = app;
